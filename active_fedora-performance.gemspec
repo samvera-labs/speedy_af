60f889f3b3571d66f0ddb831a08695b4d4ba189c
Michael Klein <mbklein@gmail.com>
2017-03-05 01:58:57 -0600

    Get rid of references to never-implemented ActiveJob pieces

--- active_fedora-performance.gemspec
+++ active_fedora-performance.gemspec
@@ -26,7 +26,6 @@ Gem::Specification.new do |s|
   s.add_dependency 'activesupport'
   s.add_development_dependency 'solr_wrapper', '~> 0.15'
   s.add_development_dependency 'fcrepo_wrapper', '~> 0.2'
-  s.add_development_dependency 'activejob'
   s.add_development_dependency 'simplecov'
   s.add_development_dependency 'coveralls'
   s.add_development_dependency 'rspec', '~> 3.0'
--- lib/active_fedora/performance.rb
+++ lib/active_fedora/performance.rb
@@ -1,14 +1,8 @@
 require 'active_fedora'
 module ActiveFedora
   module Performance
-    def self.active_job?
-      Object.const_defined?(:ActiveJob)
-    end
     autoload :IndexedContent, 'active_fedora/performance/indexed_content'
     autoload :SolrPresenter, 'active_fedora/performance/solr_presenter'
     autoload :OrderedAggregationIndex, 'active_fedora/performance/ordered_aggregation_index'
-    if active_job?
-      autoload :ReindexOrderedAggregationJob, 'active_fedora/performance/reindex_ordered_aggregation_job'
-    end
   end
 end
