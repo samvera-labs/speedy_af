5a949b26c598dcb8f4670fb564785aafdcd2d8ba
Michael Klein <mbklein@gmail.com>
2017-03-07 11:51:33 -0600

    Remove autoload for IndexedFile

--- lib/speedy_af.rb
+++ lib/speedy_af.rb
@@ -1,7 +1,6 @@
 require 'active_fedora'
 module SpeedyAF
   autoload :IndexedContent, 'speedy_af/indexed_content'
-  autoload :IndexedFile, 'speedy_af/indexed_file'
   autoload :SolrPresenter, 'speedy_af/solr_presenter'
   autoload :OrderedAggregationIndex, 'speedy_af/ordered_aggregation_index'
 end
