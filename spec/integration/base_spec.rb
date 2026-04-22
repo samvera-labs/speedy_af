76f7069d5662ac3dd42c6a5ce34adef895ffc01f
Mason Ballengee <masaball@iu.edu>
2026-04-22 14:17:21 -0400

    Handle case where id does not exist

--- spec/integration/base_spec.rb
+++ spec/integration/base_spec.rb
@@ -72,15 +72,16 @@ describe SpeedyAF::Base do
         expect { SpeedyAF::Base.find(nil) }.to raise_error(ArgumentError, "Couldn't find SpeedyAF::Base without an ID")
       end
 
+      it 'raises RecordNotFound if id is not found' do
+        expect { SpeedyAF::Base.find('foo') }.to raise_error(SpeedyAF::RecordNotFound, "Couldn't find SpeedyAF::Base with ID 'foo'")
+      end
+
       context 'proxy subclass' do
         it 'does not raise error for matching model' do
           expect { SpeedyAF::Proxy::Book.find(book.id) }.to_not raise_error
         end
         it 'raises error for mismatched model' do
           expect { SpeedyAF::Proxy::Book.find(library.id) }.to raise_error(SpeedyAF::ModelMismatch)
-        end
-        it 'returns raises ArgumentError if id is blank' do
-          expect { SpeedyAF::Proxy::Book.find(nil) }.to raise_error(ArgumentError, "Couldn't find SpeedyAF::Base without an ID")
         end
       end
     end
